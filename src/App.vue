f5722fc74f1220b49901883d74df0fa515a86e60
Joel Mitchell <joey5533a@gmail.com>
2025-09-14 09:31:22 -0500

    second commit of all files

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { ref } from 'vue'
+  const name = ref('')
+
+</script>
+
+<template>
+
+  <main class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        welcome back, <input type="text" placeholder="your name" v-model="name">
+      </h2>
+    </section>
+
+    <section class="create-todo">
+
+    </section>
+
+    <section class="todo-list">
+
+    </section>
+    
+
+  </main>
+
+</template>
